acc8b6cb997b66372f1a5b226b59f5f418838c29
Mia Gerber <mia@labs.epiuse.com>
2018-09-18 08:07:38 +0200

    Initial commit, Spring Initializer files

---
+++ src/main/java/com/dev/usermanserv/UserManServApplication.java
@@ -0,0 +1,12 @@
+package com.dev.usermanserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManServApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dev/usermanserv/UserManServApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dev.usermanserv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserManServApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
